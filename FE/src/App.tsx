bf03435032d70aaa01b3635216f366284a9f83f4
Minh Nghĩa <hako1412004@gmail.com>
2026-02-28 02:56:25 +0700

    feat: add BlogsPage component for community blog functionality
    - Implemented CreatePostCard for users to create new blog posts with images and tags.
    - Developed PostCard to display individual blog posts with like and comment features.
    - Integrated blogService for API interactions related to blog posts, comments, and likes.
    - Added ManagerSidebar component for navigation in the manager interface with user details and logout functionality.
    - Created types and interfaces for blog-related data structures.

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -14,9 +14,8 @@ import ForgotPasswordPage from './pages/HomePage/ForgotPasswordPage';
 import ResetPasswordPage from './pages/HomePage/ResetPasswordPage';
 import AuthCallbackPage from './pages/HomePage/AuthCallbackPage';
 import VerifyEmailPage from './pages/HomePage/VerifyEmailPage';
-import RecipesCooking from './pages/HomePage/RecipesCooking';
 import FarmStories from './pages/HomePage/FarmStories';
-import MarketNewsTips from './pages/HomePage/MarketNewsTips';
+import BlogsPage from './pages/HomePage/BlogsPage';
 import AboutUs from './pages/HomePage/AboutUs';
 import Profile from './pages/Customer/Profile';
 import AdminDashboard from './pages/Admin/AdminDashboard';
@@ -41,9 +40,8 @@ export default function App() {
             <Route path="/reset-password" element={<ResetPasswordPage />} />
             <Route path="/auth/callback" element={<AuthCallbackPage />} />
             <Route path="/verify-email" element={<VerifyEmailPage />} />
-            <Route path="/blogs/cooking-tips" element={<RecipesCooking />} />
-            <Route path="/blogs/green-living" element={<FarmStories />} />
-            <Route path="/blogs/news-offers" element={<MarketNewsTips />} />
+            <Route path="/blogs/green-living" element={<BlogsPage />} />
+            <Route path="/blogs/news-offers" element={<FarmStories />} />
             <Route path="/about" element={<AboutUs />} />
             <Route path="/products" element={<ProductsPage />} />
             <Route path="/products/:category" element={<ProductsPage />} />
